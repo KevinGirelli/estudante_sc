b686e772274dc1247ccb9e8ddfe0a508f3bf7c75
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-01-10 13:58:54 -0300

    Adicionando o roteamento das páginas de login e registro

---
+++ src/main/resources/estudante_sc_angular/src/app/auth/components/signup/signup-professor/signup-professor/signup-professor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-professor',
+  templateUrl: './signup-professor.component.html',
+  styleUrls: ['./signup-professor.component.scss']
+})
+export class SignupProfessorComponent {
+
+}
