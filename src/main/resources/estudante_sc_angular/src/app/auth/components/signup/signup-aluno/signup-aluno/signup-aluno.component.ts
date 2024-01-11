0f6aaa70ce1ed7d264da56a52d9f816b824b4a9b
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-01-11 10:18:09 -0300

    Add Front Signup Student

--- src/main/resources/estudante_sc_angular/src/app/auth/components/signup/signup-aluno/signup-aluno/signup-aluno.component.ts
+++ src/main/resources/estudante_sc_angular/src/app/auth/components/signup/signup-aluno/signup-aluno/signup-aluno.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-aluno',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class SignupAlunoComponent {
 
+  isSpinning: boolean = false;
+  signupForm!: FormGroup;
+  selectedDate: string = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      nome: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      cpf: [null, [Validators.required]],
+      dataNasc: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+
+      //Responsável
+      nomePai: [null, [Validators.required]],
+      cpfPai: [null, [Validators.required]],
+      phonePai: [null, [Validators.required]],
+
+      //Endereço
+      estado: [null, [Validators.required]],
+      cidade: [null, [Validators.required]],
+      cep: [null, [Validators.required]],
+      numeroResidencial: [null, [Validators.required]],
+    })
+  }
+
+  register() {
+    console.log(this.signupForm.value);
+  }
 }
