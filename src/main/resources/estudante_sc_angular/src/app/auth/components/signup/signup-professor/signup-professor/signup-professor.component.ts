7dba555b822b901a96fc02406629e2d6ee2cdc8d
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-01-13 11:18:22 -0300

    Adição do visual de registro dos professores

--- src/main/resources/estudante_sc_angular/src/app/auth/components/signup/signup-professor/signup-professor/signup-professor.component.ts
+++ src/main/resources/estudante_sc_angular/src/app/auth/components/signup/signup-professor/signup-professor/signup-professor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-professor',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class SignupProfessorComponent {
 
+  isSpinning: boolean = false;
+  signupForm!: FormGroup;
+  selectedDate: string = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      nome: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      cpf: [null, [Validators.required]],
+      dataNasc: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+
+      //Endereço
+      estado: [null, [Validators.required]],
+      cidade: [null, [Validators.required]],
+      cep: [null, [Validators.required]],
+      numeroResidencial: [null, [Validators.required]],
+    })
+  }
+
+  register() {
+    console.log(this.signupForm.value);
+  }
 }
